3af83df1d4a194d2ab9a6cc979ee32fe7b7b6213
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-14 16:01:56 +0900

    計算問題

---
+++ ex01/quiz.py
@@ -0,0 +1,21 @@
+import random
+mondai = ["3+4は？","3*3+3は？","6/3は？"]
+kotae = [["7","なな","ナナ","七"],["12","じゅうに","十二","ジュウニ"],["2","に","ニ","二"]]
+a = 0
+def syutudai():
+    a = random.randint(0,2)
+    print(f"問題:{mondai[a]}")
+    n = kotae[a]
+    return n
+
+def kaito(x):
+    n = input("入力:")
+    if n in x:
+        print("正解")
+    else:
+        print("不正解")
+
+def main():
+    s = syutudai()
+    kaito(s)
+
