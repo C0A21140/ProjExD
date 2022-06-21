47949b5707ddb44211453a18e114e89bb2e58be6
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-21 14:31:18 +0900

    calc 問2

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,7 +1,22 @@
+from logging import root
 import tkinter as tk
 
-if __name__ == "__main__"
-root = tk.Tk()
-root.title("電卓")
-root.geometry("300x500")
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("電卓")
+    root.geometry("300x500")
+    c = 0
+    r = 0
+    for i in range(9, -1, -1):
+
+        btn = tk.Button(root, text = f"{i}",
+                        width = 4, height = 2,
+                        font = ("Times New Roman", 30)
+                        )
+        
+        btn.grid(row = r, column = c)
+        c += 1
+        if (i-1)%3 == 0:
+            r += 1
+            c = 0
+    root.mainloop()
