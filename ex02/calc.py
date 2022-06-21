3d31b1d742a4b17d827bf9e192e2709f24cafcaa
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-21 14:39:56 +0900

    calc 問3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,10 @@
 from logging import root
 import tkinter as tk
-
+import tkinter.messagebox as tkm
+def btn_click(event):
+    botan = event.widget
+    txt = botan["text"]
+    tkm.showinfo("確認", f"[{txt}]ボタンが押されました")
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
@@ -15,6 +19,7 @@ if __name__ == "__main__":
                         )
         
         btn.grid(row = r, column = c)
+        btn.bind("<1>", btn_click)
         c += 1
         if (i-1)%3 == 0:
             r += 1
