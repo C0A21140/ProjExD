56d729b3cc7e441336a89698bf0b8ce7ddc63ee4
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-28 17:43:43 +0900

    コメント#9 再

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,31 +1,32 @@
 import tkinter as tk
 #import tkinter.messagebox as tkm
 import maze_maker as make
-def key_down(event):
+
+def key_down(event):#ボタンを押している時
     global key
     key = event.keysym
     #tkm.showinfo("キー押下", f"{key}が押されました")
 
-def key_up(event):
+def key_up(event):#ボタンを押していないとき
     global key
     key = ""
     tori["file"] = "fig/0.png"
 
-def main_proc():#移動する方向によって画像が変わる
+def main_proc():#移動する方向によって画像が変わる　　移動機能の関数
     global cx, cy, mx, my, tori
-    if key == "Up":
+    if key == "Up":#上に動く時は画像6
         if meiro[my-1][mx] == 0:
             tori["file"] = "fig/6.png"
             my -= 1
-    elif key == "Down":
+    elif key == "Down":#下に動くときは画像7
         if meiro[my+1][mx] == 0:
             tori["file"] = "fig/8.png"
             my += 1
-    elif key == "Right":
+    elif key == "Right":#右に動くときは画像8
         if meiro[my][mx+1] == 0:
             tori["file"] = "fig/2.png"
             mx += 1
-    elif key == "Left":
+    elif key == "Left":#左に動くときは画像3
         if meiro[my][mx-1] == 0:
             tori["file"] = "fig/3.png"
             mx -= 1
