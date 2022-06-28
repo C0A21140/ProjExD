fab8e45af50c598472a64eb3dba7f04efaee30b0
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-28 14:26:14 +0900

    行程３

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,9 +4,12 @@ import tkinter.messagebox as tkm
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    window = tk.Canvas(root,
+    canvas = tk.Canvas(root,
                        width = 1500,
                        height = 900,
                        bg = "black")
-    window.pack()
+    canvas.pack()
+    tori = tk.PhotoImage(file = "fig/0.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image = tori, tag = "tori")
     root.mainloop()
