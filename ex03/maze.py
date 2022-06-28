0da4ff9e6182b332bfe55f94d538a90a57d854c1
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-28 14:55:20 +0900

    行程７

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,7 +3,24 @@ import tkinter.messagebox as tkm
 def key_down(event):
     global key
     key = event.keysym
-    tkm.showinfo("キー押下", f"{key}が押されました")
+    #tkm.showinfo("キー押下", f"{key}が押されました")
+
+def key_up(event):
+    global key
+    key = ""
+    
+def main_proc():
+    global cx, cy, key
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Right":
+        cx += 20
+    elif key == "Left":
+        cx -= 20
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -19,4 +36,6 @@ if __name__ == "__main__":
     canvas.create_image(cx, cy, image = tori, tag = "tori")
     key = ""
     root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+    root.after(100, main_proc)
     root.mainloop()
