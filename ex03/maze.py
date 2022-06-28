15aa3e75eb49ac6cd893b9548c0549be22174761
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-28 14:19:46 +0900

    行程２修正

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,5 +4,9 @@ import tkinter.messagebox as tkm
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    root.geometry("1500x900")
+    window = tk.Canvas(root,
+                       width = 1500,
+                       height = 900,
+                       bg = "black")
+    window.pack()
     root.mainloop()
