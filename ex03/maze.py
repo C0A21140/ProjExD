7dd34bedbafd0e840054b2fa1bb312594a3b5ff4
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-28 14:15:37 +0900

    行程１と２

---
+++ ex03/maze.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    root.geometry("1500x900")
+    root.mainloop()
