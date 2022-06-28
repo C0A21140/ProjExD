f77c5aef8d9043a737d87951bb8d4e7dcf7fdc92
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-06-28 14:35:52 +0900

    行程４５６

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+def key_down(event):
+    global key
+    key = event.keysym
+    tkm.showinfo("キー押下", f"{key}が押されました")
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -7,9 +11,12 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root,
                        width = 1500,
                        height = 900,
-                       bg = "black")
+                       bg = "black"
+                       )
     canvas.pack()
     tori = tk.PhotoImage(file = "fig/0.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image = tori, tag = "tori")
+    key = ""
+    root.bind("<KeyPress>", key_down)
     root.mainloop()
