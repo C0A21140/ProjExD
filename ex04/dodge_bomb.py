cb5cd32ab44f1f0ed4db12e308c7092f1689c50a
Gudo3861 <c0a2114017@edu.teu.ac.jp>
2022-07-05 14:24:56 +0900

    逃げろこうかとん問３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,3 +1,4 @@
+from http.client import TEMPORARY_REDIRECT
 import pygame as pg
 import sys
 
@@ -11,13 +12,18 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_rect = bg_img.get_rect()
     screen. blit(bg_img, bg_rect)
+
+    tori_img = pg.image.load("fig/6.png")
+    tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
+    tori_rect = tori_img.get_rect()
+    tori_rect.center = 900, 400
     
-    #tori_rect = tori_img.get_rect()
-    #tori_rect.center = 700, 400
-    #screen.blit(tori_img, tori_rect)
+
+
 
     while(True):
         screen. blit(bg_img, bg_rect)
+        screen.blit(tori_img, tori_rect)
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
